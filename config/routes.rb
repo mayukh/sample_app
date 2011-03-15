d739f827da9913643a5b90de88da5d597fdd1940
Mayukh <mayukhm@gmail.com>
2011-03-15 02:49:50 -0400

    Added layout link tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 SampleApp::Application.routes.draw do
 
+  get "users/new"
+
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   root :to => "pages#home"
@@ -7,6 +9,7 @@ SampleApp::Application.routes.draw do
   match '/contact', :to => 'pages#contact'
   match '/about',   :to => 'pages#about'
   match '/help',    :to => 'pages#help'
+  match '/signup',  :to => 'users#new'
   
   
   # The priority is based upon order of creation:
