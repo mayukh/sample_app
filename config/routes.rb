ea795dd73631550219fecc92e0b14ef59126ee42
Mayukh <mayukhm@gmail.com>
2011-03-15 02:22:10 -0400

    Added custom paths, fixed up routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 SampleApp::Application.routes.draw do
 
-  get "pages/home"
-  get "pages/contact"
-  get "pages/about"
+  # You can have the root of your site routed with "root"
+  # just remember to delete public/index.html.
+  root :to => "pages#home"
+
+  match '/contact', :to => 'pages#contact'
+  match '/about',   :to => 'pages#about'
+  match '/help',    :to => 'pages#help'
+  
   
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -51,9 +56,7 @@ SampleApp::Application.routes.draw do
   #     resources :products
   #   end
 
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+
 
   # See how all your routes lay out with "rake routes"
 
