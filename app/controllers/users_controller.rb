21c8453dfba6b9aec657f230b3f2197dfc18e5a7
Mayukh <mayukhm@gmail.com>
2011-03-17 00:26:50 -0400

    Finished the sign out and integration tests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
     if @user.save
+      sign_in @user  #After signing up we should create a new session
       #Handle a successful save
       #send it to the show page users/1  
       redirect_to @user , :flash => {:success => "Welcome to the Sample App!"} 
