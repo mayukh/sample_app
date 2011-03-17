959ab0ef9fa87c3767ae76bd2f90556e3b49888c
Mayukh <mayukhm@gmail.com>
2011-03-17 18:37:50 -0400

    Working index page

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -3,6 +3,47 @@ require 'spec_helper'
 describe UsersController do
   render_views
   
+  describe "GET 'index'" do
+    
+    describe "for non-signed-in users" do
+      it "should deny access" do
+        get :index
+        response.should redirect_to(signin_path)
+      end
+    end #non signed-in
+    
+     describe "for signed-in users" do
+      
+       before(:each) do
+        @user = test_sign_in(Factory(:user))
+        Factory(:user, :email => "another@example.com")
+        Factory(:user, :email => "another@example.net")
+       end
+        it "should be a success" do
+          get :index
+          response.should be_success
+          #response.should redirect_to(signin_path)
+        end
+        
+        it "should have the right title" do
+          get :index
+          response.should have_selector("title", 
+                                         :content => "Users")
+        end
+        
+        it "should have an element from each user" do
+          get :index
+          User.all.each do |user|
+            response.should have_selector("li",:content => user.name)
+          end
+        end
+
+
+      end #signed-in
+    
+  end #GET 
+  
+  
   describe "GET 'show'" do
     
    before(:each) do  
